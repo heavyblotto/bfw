44e4ee2a541eee7442fd2c70c355fb3f7b1a48bd
jk <jk2015@protonmail.com>
2024-09-29 12:32:55 -0400

    Fixing Vercel deployment issues
    On branch fixing-auth-on-vercel
    Changes to be committed:
    	new file:   src/lib/prismadb.ts
    	modified:   src/pages/api/auth/[...nextauth].ts
    	modified:   src/pages/api/auth/register.ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,8 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
 import bcrypt from "bcrypt"
-
-const prisma = new PrismaClient()
+import prisma from "../../../lib/prismadb"
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
